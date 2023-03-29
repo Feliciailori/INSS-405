83363a15eb0f0f004750c357a512a594d18483f9
felic <felicia.a.ilori@gmial.com>
2023-03-29 19:14:00 -0400

    Lab4

--- Lab3/Lab3-part4.py
+++ Lab3/Lab3-part4.py
@@ -5,3 +5,11 @@ elif(int(number)%5==0 and int(number)%3!=0 and int(number)%15!=0):
     print('Bowie State')
 elif(int(number)%15==0):
     print('Bowie State University')
+
+#another method is to put the syntax from bottom to top. That is start with 15 instead of 5
+# if(int(number)%15==0)
+#     print('Bowie State University')
+# if(int(number)%5==0)
+#     print('Bowie State')
+# if(int(number)%3==0):
+#     print('Bowie')
